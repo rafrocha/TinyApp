be736ffd5d2dd33510d675ba174d36aafaba32d2
Rafael <rafaelmarianorocha@gmail.com>
2018-07-10 23:01:35 +0000

    Adds /u/ GET request with shortURL.

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ var PORT = 8080; // default port 8080
 app.set("view engine", "ejs")
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -37,14 +37,21 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
+  console.log(res.statusCode);
   const newShortURL = generateRandomString();
-  urlDatabase[newShortURL] = req.body.longURL;  // debug statement to see POST parameters
-  res.redirect(`/urls/${newShortURL}`);         // Respond with 'Ok' (we will replace this)
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect(`/urls/${newShortURL}`);
 });
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log(res.statusCode);
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls.json", (req, res) => {

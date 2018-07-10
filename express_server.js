ec00ab76c7e4874b005d2da85d018945450a98fc
Rafael <rafaelmarianorocha@gmail.com>
2018-07-10 22:13:45 +0000

    Adds main server js file and views directory.

---
+++ express_server.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+app.set("view engine", "ejs")
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString() {
+  var text = "";
+  var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+var tagline = "codes";
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase, tagline: tagline };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;  // debug statement to see POST parameters
+  res.redirect(`/urls/${newShortURL}`);         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
